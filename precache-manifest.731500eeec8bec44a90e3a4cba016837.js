605b90e4afa5bc0599dc9f0631bc1b2d1a6bffe9
장현웅 <jhw81111@gmail.com>
2021-01-06 22:29:46 +0900

    Updates

---
+++ precache-manifest.731500eeec8bec44a90e3a4cba016837.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b82b81f66b24743793f21f85eaaa422",
+    "url": "/neovision21.github.io/index.html"
+  },
+  {
+    "revision": "f9a8ba76bca07abdbd10",
+    "url": "/neovision21.github.io/static/css/main.9b1431d8.chunk.css"
+  },
+  {
+    "revision": "eb896c6dac7d81965713",
+    "url": "/neovision21.github.io/static/js/2.01c521f1.chunk.js"
+  },
+  {
+    "revision": "28f29a598e465553cfeea810767399d3",
+    "url": "/neovision21.github.io/static/js/2.01c521f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9a8ba76bca07abdbd10",
+    "url": "/neovision21.github.io/static/js/main.2b013218.chunk.js"
+  },
+  {
+    "revision": "5ada63b40546ec5ff8c4",
+    "url": "/neovision21.github.io/static/js/runtime-main.9be0dc0a.js"
+  }
+]);
--- src/pages/Main.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-const Main: React.FC = () => {
-  return <div>aaaa</div>;
-};
-export default Main;
